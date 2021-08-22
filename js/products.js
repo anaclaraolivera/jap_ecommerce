d9b609f5e3ca6bbb0f4324a576bdc361fff49c0e
anaclaraolivera <acolivera13@gmail.com>
2021-08-22 13:15:57 -0300

    Ediciones de texto

--- js/products.js
+++ js/products.js
@@ -2,5 +2,41 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok"){
+            console.log (resultObj.data);
+           // sortAndShowCategories(ORDER_ASC_BY_NAME, resultObj.data);
+        }
+    });
 });
+
+
+
+
+// function showCategoriesList(){
+
+//     let htmlContentToAppend = "";
+//     for(let i = 0; i < currentProductsArray.length; i++){
+//         let category = currentProductsArray[i];
+
+//             htmlContentToAppend += `
+//             <a href="category-info.html" class="list-group-item list-group-item-action">
+//                 <div class="row">
+//                     <div class="col-3">
+//                         <img src="` + category.imgSrc + `" alt="` + category.description + `" class="img-thumbnail">
+//                     </div>
+//                     <div class="col">
+//                         <div class="d-flex w-100 justify-content-between">
+//                             <h4 class="mb-1">`+ category.name +`</h4>
+//                             <small class="text-muted">` + category.productCount + ` artículos</small>
+//                         </div>
+//                         <p class="mb-1">` + category.description + `</p>
+//                     </div>
+//                 </div>
+//             </a>
+//             `
+//         }
+
+//         document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
+//     }
+// }
