27642b97ea46e4383f824269e1754e37955bd8a4
anaclaraolivera <acolivera13@gmail.com>
2021-09-03 20:47:59 -0300

    muchas cosas arregladas
    falta arreglar algunas cosas, pero vamos bieeeen

--- js/index.js
+++ js/index.js
@@ -3,17 +3,37 @@
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
     document.getElementById("boton-login").addEventListener("click", function(){
-        goToTheRealIndex();
+        guardarUsuario();
     });
 });
 
-// Esta función redirecciona al inicio real de la página web siempre y cuando
-// se haya ingresado información de usuario y contraseña
-function goToTheRealIndex(){
-    if(document.getElementById("password").value != "" &&  document.getElementById("email").value != "")  {
-       // aquí te redirecciona al inicio real del ecommerce si se cargó la info de usuario y contraseña
-        window.location.href = 'afterIndex.html';
-    } else {
-        alert("Ingresar usuario y contraseña");
+// // Esta función redirecciona al inicio real de la página web siempre y cuando
+// // se haya ingresado información de usuario y contraseña
+// function goToTheRealIndex(){
+//     if(document.getElementById("contraseña").value != "" &&  document.getElementById("usuario").value != "")  {
+//        // aquí te redirecciona al inicio real del ecommerce si se cargó la info de usuario y contraseña
+//        // window.location.href = 'afterIndex.html';
+//     }
+//     // ¿debería sacarlo porque está el de abajo?
+//      else {
+//         alert("Ingresar usuario y contraseña");
+//     }
+// }
+
+function guardarUsuario(){
+ 
+    let usuario = document.getElementById("usuario").value;
+    let contraseña = document.getElementById("contraseña").value;
+    if(usuario !=="" && contraseña!== ""){
+        localStorage.setItem('usuario', usuario);
+        window.location.href="afterIndex.html";
     }
+    else{
+        alert("debe completar los campos");
+    }
+
 }
+
+
+
+
--- js/products.js
+++ js/products.js
@@ -1,71 +1,62 @@
-var currentProductsArray = [];
 const ORDER_ASC_BY_PRICE = "UP";
 const ORDER_DESC_BY_PRICE = "DOWN";
-const ORDER_BY_PROD_SOLD = "Sold.";
+const ORDER_BY_PROD_SOLD = "Sold";
+var currentProductsArray = [];
+var currentSortCriteria = undefined;
 var minCount = undefined;
 var maxCount = undefined;
 
 
-//Clasificar las categorías en función de criteria y de array
-function sortProducts(criteria, array){
+//Clasificar los productos en función de criteria y de array
+function sortProducts(criteria, array) {
     //Se crea una variable que es un array vacío donde se cargará lo que se ordene
     let result = [];
     //Condición cuando lo querés ascendente 
-    if (criteria === ORDER_ASC_BY_PRICE)
-    {
+    if (criteria === ORDER_DESC_BY_PRICE) {
         // Se carga en result el array ya ordenado por criteria
-        result = array.sort(function(a, b) {
+        result = array.sort(function (a, b) {
             // Compara de uno contra uno y devuelve el menor 
-            if ( a.cost < b.cost ){ return -1; }
-            if ( a.cost > b.cost ){ return 1; }
+            if (a.cost < b.cost) { return -1; }
+            if (a.cost > b.cost) { return 1; }
             return 0;
         });
         //Condición cuando lo querés por orden descendente  
-    }else if (criteria === ORDER_DESC_BY_PRICE){
-       // Se carga en result el array ya ordenado por criteria
-        result = array.sort(function(a, b) {
+    } else if (criteria === ORDER_ASC_BY_PRICE) {
+        // Se carga en result el array ya ordenado por criteria
+        result = array.sort(function (a, b) {
             // Compara de uno contra uno y devuelve el mayor
-            if ( a.cost > b.cost ){ return -1; }
-            if ( a.cost < b.cost ){ return 1; }
+            if (a.cost > b.cost) { return -1; }
+            if (a.cost < b.cost) { return 1; }
             return 0;
         });
         // Condición cuando querés que se ordene por productos vendidos
-    }else if (criteria === ORDER_BY_PROD_SOLD){
-           result = array.sort(function(a, b) {
+    } else if (criteria === ORDER_BY_PROD_SOLD) {
+        result = array.sort(function (a, b) {
             // Variables que almacenan la cantidad de productos en enteros
             let aCount = parseInt(a.soldCount);
             let bCount = parseInt(b.soldCount);
             // Compara los valores de cantidad y retorna el mayor
-            if ( aCount > bCount ){ return -1; }
-            if ( aCount < bCount ){ return 1; }
+            if (aCount > bCount) { return -1; }
+            if (aCount < bCount) { return 1; }
             return 0;
         });
     }
-// Devuelve el resultado que es un array 
+    // Devuelve el resultado que es un array 
     return result;
 }
 
-
-
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
-    getJSONData(PRODUCTS_URL).then(function (resultObj) {
-        if (resultObj.status === "ok") {
-            currentProductsArray = resultObj.data;
-            showProductoList();
-        }
-    });
-   
-});
-
 // Función que sirve para mostrar los productos en pantalla
-function showProductoList() {
+function showProductsList() {
+
     let htmlContentToAppend = "";
     for (let i = 0; i < currentProductsArray.length; i++) {
         let product = currentProductsArray[i];
-        htmlContentToAppend += `
+//Cuando se llama a la función showProductsList si minCount y maxCount no son undefined se van a mostrar los 
+//productos comprendidos en ese rango
+        if (((minCount == undefined) || (minCount != undefined && parseInt(product.cost) >= minCount)) &&
+            ((maxCount == undefined) || (maxCount != undefined && parseInt(product.cost) <= maxCount))) {
+            //construye el html
+                htmlContentToAppend += `
             <a href="" class="list-group-item list-group-item-action">
                 <div class="row">
                     <div class="col-3">
@@ -82,56 +73,80 @@ function showProductoList() {
                 </div>
             </a>
             `
+        }
+
     }
     document.getElementById("prod-list-container").innerHTML = htmlContentToAppend;
 }
 
+// función que ordena y muestra los productos
+function sortAndShowProducts(sortCriteria, productsArray) {
+    currentSortCriteria = sortCriteria;
+    // si el array no está vacío lo copiamos en una variable auxiliar  
+    if (productsArray != undefined) {
+        currentProductsArray = productsArray;
+    }
+    // se ordena la variable auxiliar que se creó anteriormente 
+    currentProductsArray = sortProducts(currentSortCriteria, currentProductsArray);
 
-document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData(PRODUCTS_URL).then(function(resultObj){
-        if (resultObj.status === "ok"){
-            sortAndShowCategories(ORDER_ASC_BY_PRICE, resultObj.data);
+    //Muestro las categorías ordenadas
+    showProductsList();
+}
+
+
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            sortAndShowProducts(ORDER_ASC_BY_PRICE, resultObj.data);
         }
     });
-    document.getElementById("sortAsc").addEventListener("click", function(){
-        sortAndShowCategories(ORDER_ASC_BY_PRICE);
+    document.getElementById("sortAsc").addEventListener("click", function () {
+        sortAndShowProducts(ORDER_ASC_BY_PRICE);
     });
 
-    document.getElementById("sortDesc").addEventListener("click", function(){
-        sortAndShowCategories(ORDER_DESC_BY_PRICE);
+    document.getElementById("sortDesc").addEventListener("click", function () {
+        sortAndShowProducts(ORDER_DESC_BY_PRICE);
     });
 
-    document.getElementById("sortBySold").addEventListener("click", function(){
-        sortAndShowCategories(ORDER_BY_PROD_SOLD);
+    document.getElementById("sortBySold").addEventListener("click", function () {
+        sortAndShowProducts(ORDER_BY_PROD_SOLD);
     });
-//Ahí termina de identificar botones y comienza con el filtro 
-    document.getElementById("clearRangeFilter").addEventListener("click", function(){
+ 
+    //Limpia las variables globales de minCount y maxCount 
+    document.getElementById("clearRangeFilter").addEventListener("click", function () {
         document.getElementById("rangeFilterCountMin").value = "";
         document.getElementById("rangeFilterCountMax").value = "";
 
         minCount = undefined;
         maxCount = undefined;
 
-        showCategoriesList();
+        showProductsList();
+    });
 
-document.getElementById("rangeFilterCount").addEventListener("click", function(){
-    //Obtengo el mínimo y máximo de los intervalos para filtrar por rango de precio 
-    minCount = document.getElementById("rangeFilterCountMin").value;
-    maxCount = document.getElementById("rangeFilterCountMax").value;
+    document.getElementById("rangeFilterCount").addEventListener("click", function () {
+        //Obtengo el mínimo y máximo de los intervalos para filtrar por rango de precio 
+        minCount = document.getElementById("rangeFilterCountMin").value;
+        maxCount = document.getElementById("rangeFilterCountMax").value;
+//validar que los valores de minCount y maxCount sean números positivos y luego los asigna a las variables globales
+        if ((minCount != undefined) && (minCount != "") && (parseInt(minCount)) >= 0) {
+            minCount = parseInt(minCount);
+        }
+        else {
+            minCount = undefined;
+        }
 
-    if ((minCount != undefined) && (minCount != "") && (parseInt(minCount)) >= 0){
-        minCount = parseInt(minCount);
-    }
-    else{
-        minCount = undefined;
-    }
+        if ((maxCount != undefined) && (maxCount != "") && (parseInt(maxCount)) >= 0) {
+            maxCount = parseInt(maxCount);
+        }
+        else {
+            maxCount = undefined;
+        }
+        //Muestra los productos según el rango que se haya definido en minCount y maxCount
+        showProductsList();
+    });
+});
 
-    if ((maxCount != undefined) && (maxCount != "") && (parseInt(maxCount)) >= 0){
-        maxCount = parseInt(maxCount);
-    }
-    else{
-        maxCount = undefined;
-    }
 
-    showProductoList();
-}))
