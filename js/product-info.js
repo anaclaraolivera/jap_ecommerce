a49717c10cc0dc1643ac8c3174787029da95e906
anaclaraolivera <acolivera13@gmail.com>
2021-09-12 12:23:23 -0300

    redirigir a información de productos
    hecho

--- js/product-info.js
+++ js/product-info.js
@@ -1,6 +1,54 @@
+var product = {};
+
+function mostrarImagenes(array){
+
+    let htmlContentToAppend = "";
+
+    for(let i = 0; i < array.length; i++){
+        let imageSrc = array[i];
+
+        htmlContentToAppend += `
+        <div class="col-lg-3 col-md-4 col-6">
+            <div class="d-block mb-4 h-100">
+                <img class="img-fluid img-thumbnail" src="` + imageSrc + `" alt="">
+            </div>
+        </div>
+        `
+
+        document.getElementById("ImagenesProducto").innerHTML = htmlContentToAppend;
+    }
+}
+
+
+
+
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCT_INFO_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            product = resultObj.data;
 
+            let productNameHTML = document.getElementById("productName");
+            let productDescriptionHTML = document.getElementById("productDescription");
+            let productCostHTML = document.getElementById("productCost");
+            let productCurrencyHTML = document.getElementById("productCurrency");
+            let productSoldCountHTML = document.getElementById("productSoldCount");
+            let productCategoryHTML = document.getElementById("productCategory");
+        
+            productNameHTML.innerHTML = product.name;
+            productDescriptionHTML.innerHTML = product.description;
+            productCostHTML.innerHTML = product.cost;
+            productCurrencyHTML.innerHTML = product.currency;
+            productSoldCountHTML.innerHTML = product.soldCount;
+            productCategoryHTML.innerHTML = product.category;
+
+            //Muestro las imagenes en forma de galería
+            mostrarImagenes(product.images);
+        }
+    });
 });
--- js/products.js
+++ js/products.js
@@ -56,7 +56,7 @@ function showProductsList() {
             ((maxCount == undefined) || (maxCount != undefined && parseInt(product.cost) <= maxCount))) {
             //construye el html
                 htmlContentToAppend += `
-            <a href="" class="list-group-item list-group-item-action">
+            <a href="product-info.html" class="list-group-item list-group-item-action">
                 <div class="row">
                     <div class="col-3">
                         <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
