aab33d73d002d06278d2e6fed7e3da3cc0a31f54
anaclaraolivera <acolivera13@gmail.com>
2021-09-12 23:51:11 -0300

    arreglando y rompiendo cosas

--- js/comentarios-productos.js
+++ js/comentarios-productos.js
@@ -1,6 +1,5 @@
 var comentarios = [];
 
-// No sé cómo hacer esto 
 function mostrarComentarios(lista){
 
     let AgregarAlHTML = "";
@@ -8,27 +7,65 @@ function mostrarComentarios(lista){
         let comentario = lista[i];
 
         AgregarAlHTML += 
-
-
         `
       <div class= "card">
            <div class="card-body">
                 <h5 class="card-title">Usuario: `+ comentario.user + `</h5>
                 <p class="card-text">`+ comentario.description + `</p>
-                <div class="card-body text-success">
-                <p class="card-text">Puntaje: `+ comentario.score +`/5 </p>
-                </div>
+                <div id="puntaje"></div>
            </div>
            <div class="card-footer">
                 <small class="text-muted">`+ comentario.dateTime + `</small>
            </div>
      </div>
-
       `
-    
         document.getElementById("container-comentarios").innerHTML = AgregarAlHTML;
     }
 }
+
+//no sé :(
+function puntajeComentarios (lista){
+
+    let AgregarPuntaje = "";
+
+    for (let i = 0; i < lista.length; i++) {
+        let item = lista[i];
+        if (item.score === 5) {
+            AgregarPuntaje += `
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            `
+            document.getElementById("puntaje").innerHTML = AgregarPuntaje;
+
+        } else if (item.score === 4) {
+            AgregarPuntaje += `
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star"></span>
+            `
+            document.getElementById("puntaje").innerHTML = AgregarPuntaje;
+
+        } else if (item.score === 3) {
+            AgregarPuntaje += `
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star"></span>
+            <span class="fa fa-star"></span>
+            `
+            document.getElementById("puntaje").innerHTML = AgregarPuntaje;
+            
+        }
+    }
+}
+
+
+
 
 
 
@@ -39,7 +76,13 @@ document.addEventListener("DOMContentLoaded", function (e) {
             comentarios = resultado.data;
             //muestra los comentarios
             mostrarComentarios(comentarios);
+            puntajeComentarios(comentarios);
+            
         }
 
     });
 });
+
+
+//<div class="card-body text-success">
+//<p class="card-text" id="puntaje">Puntaje: `+ comentario.score +`/5 </p>
