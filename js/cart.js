907edc2562d7cc31a25139ec8b4b63eccf3e2330
anaclaraolivera <acolivera13@gmail.com>
2021-10-06 21:48:09 -0300

    comienzo de entrega
    Se empieza a crear la función showCarrito() que será la que muestre en el HTML. y se hace una petición para obtener los datos del json. Aún no funciona, algún problema al obtener los datos del json

--- js/cart.js
+++ js/cart.js
@@ -1,7 +1,36 @@
 
-  //Función que se ejecuta una vez que se haya lanzado el evento de
-  //que el documento se encuentra cargado, es decir, se encuentran todos los
-  //elementos HTML presentes.
-  document.addEventListener("DOMContentLoaded", function (e) {
-  
+let datosCarrito = {};
+
+
+function showCarrito() {
+  let articulosCarrito = datosCarrito.articles;
+  console.log(articulosCarrito.value)
+  let contenidoHTML = "";
+  for (let i=0; i > articulosCarrito.length; i++) {
+    contenidoHTML += `
+    <tr>
+        <th scope="row">${articulosCarrito.name}</th>
+        <td>${articulosCarrito.currency}</td>
+        <td>${articulosCarrito.unitCost}</td>
+        <td>${articulosCarrito.count}</td>
+        <td> </td>
+      </tr>
+    `
+    }
+    document.getElementById("carrito-tabla").innerHTML = contenidoHTML;
+  }
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
+document.addEventListener("DOMContentLoaded", function (e) {
+  getJSONData(CART_2PROD_INFO).then(function (jsonCarrito) {
+    if (jsonCarrito.status === "ok") {
+      datosCarrito = jsonCarrito.data;
+      
+    }
+    showCarrito(datosCarrito);
   });
+});
+
+
+  
