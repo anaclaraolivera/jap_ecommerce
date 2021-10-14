1934b7986288fdddf088feb7ce62bb47d0bac053
anaclaraolivera <acolivera13@gmail.com>
2021-10-13 21:12:54 -0300

    Se arreglan cosas del carrito
    Se agrega para que el total cambie si es en pesos o en dólares

--- js/cart.js
+++ js/cart.js
@@ -1,82 +1,89 @@
 let allArticles = [];
+let subtotalPesos = 0;
+let subtotalDolares = 0;
+
+const DOLARES = "USD";
+const PESOS = "UYU";
+
 
 function showCarrito() {
-  
+
   let contenidoHTML = "";
 
-  for (let i=0; i < allArticles.length; i++) {
+  for (let i = 0; i < allArticles.length; i++) {
     article = allArticles[i];
-   
-    contenidoHTML += `  
-    <tr>
-    <td><img src =${article.src} style="width:75px;height:75px;"></td>
-    <th scope="row">${article.name}</th>
-    <td> 
-    <select id= "currency">
 
-    <option id="usd">USD</option>
-    
-    <option id="uyu">UYU</option>
-    
-    </select>
-    </td>
+    contenidoHTML += `  
+    <tr class= "espacio-columnas">
+    <td><img src =${article.src} style="width:75px;height:75px;"></td>
+    <td scope="row">${article.name}</th>
+    <td>${article.currency}</td>
     <td>${article.unitCost}</td>
-    <td><input id= "cantidad-producto${i}" type="number" value= "${article.count}" onchange="actualizarSubtotal()"></td>
+    <td class= "alineado"><input id= "cantidad-producto${i}" type="number" style="width: 35px;" value= "${article.count}" onchange="actualizarSubtotal()"></td>
     <td id="subtotal-producto${i}">${article.unitCost * article.count}</td>
     </tr>
      `
-    
-    }
-    document.getElementById("carrito-tabla").innerHTML = contenidoHTML;
+
   }
-// function showCarrito() {
-  
-//   let contenidoHTML = "";
+  document.getElementById("carrito-tabla").innerHTML += contenidoHTML;
+}
 
-//   for (let i=0; i < allArticles.length; i++) {
-//     article = allArticles[i];
-   
-//     contenidoHTML += `  
-//     <tr>
-//     <td><img src =${article.src} style="width:75px;height:75px;"></td>
-//     <th scope="row">${article.name}</th>
-//     <td>${article.currency}></td>
-//     <td>${article.unitCost}</td>
-//     <td><input id= "cantidad-producto${i}" type="number" value= "${article.count}" onchange="actualizarSubtotal()"></td>
-//     <td id="subtotal-producto${i}">${article.unitCost * article.count}</td>
-//     </tr>
-//      `
-    
-//     }
-//     document.getElementById("carrito-tabla").innerHTML = contenidoHTML;
-//   }
+function actualizarSubtotal() {
 
-  function actualizarSubtotal(){
+
+  for (let i = 0; i < allArticles.length; i++) {
     let cantidad = 0;
     let costo = 0;
 
-    for (let i=0; i < allArticles.length; i++) {
+    cantidad = document.getElementById("cantidad-producto" + i).value;
+    costo = allArticles[i].unitCost;
 
-     cantidad = document.getElementById("cantidad-producto"+i+"").value;
-     costo = allArticles[i].unitCost;
-    
-    if (cantidad<=0){
-     
-     document.getElementById("cantidad-producto"+i).value = 0;
-     
+    if (cantidad <= 0) {
+
+      document.getElementById("cantidad-producto" + i).value = 0;
+      document.getElementById("subtotal-producto" + i).innerText = 0;
+
     }
     else {
-      document.getElementById("subtotal-producto"+i).innerText = costo*cantidad;
+      document.getElementById("subtotal-producto" + i).innerText = costo * cantidad;
+
     }
   }
+  calcularSubtotal();
+}
 
+function calcularSubtotal() {
+  subtotalPesos= 0;
+  subtotalDolares= 0;
+
+  for (let i = 0; i < allArticles.length; i++) {
+
+    cantidad = document.getElementById("cantidad-producto" + i).value;
+    costo = allArticles[i].unitCost;
+
+    if (allArticles[i].currency === PESOS) {
+      subtotalPesos += costo * cantidad;
+      subtotalDolares += (costo * cantidad) / 40;
+    }
+    else {
+      subtotalPesos += (costo * cantidad) * 40;
+      subtotalDolares += costo * cantidad;
+    }
   }
-  // function cambiarCurrency(){
-  //   currency = "";
-  //   for (let i=0; i < allArticles.length; i++) {
+  document.getElementById("total-pago-dolares").innerText = subtotalDolares;
+  document.getElementById("total-pago-pesos").innerText = subtotalPesos;
+}
 
-  //   }
-  // }
+function selectCurrency(option) {
+  if (option === DOLARES) {
+    document.getElementById("total-pago-dolares").style.display = "block";
+    document.getElementById("total-pago-pesos").style.display = "none";
+  }
+  else {
+    document.getElementById("total-pago-pesos").style.display = "block";
+    document.getElementById("total-pago-dolares").style.display = "none";
+  }
+}
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
@@ -84,11 +91,20 @@ function showCarrito() {
 document.addEventListener("DOMContentLoaded", function (e) {
   getJSONData(CART_2PROD_INFO).then(function (jsonCarrito) {
     if (jsonCarrito.status === "ok") {
-     allArticles = jsonCarrito.data.articles;
+      allArticles = jsonCarrito.data.articles;
     }
     showCarrito();
+    calcularSubtotal();
+
+  });
+  document.getElementById("pesos").addEventListener("click", function () {
+    selectCurrency(PESOS);
+  });
+  document.getElementById("dólares").addEventListener("click", function () {
+    selectCurrency(DOLARES);
   });
 });
 
 
-  
+
+
