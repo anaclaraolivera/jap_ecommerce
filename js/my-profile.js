0d821db2f82802b06d6bc74275b2128520646edd
anaclaraolivera <acolivera13@gmail.com>
2021-10-27 21:30:10 -0300

    Se arreglan cosas
    Se unen dos funciones
    Se logra que la tarjeta de perfil se vea cuando se recarga la página

--- js/my-profile.js
+++ js/my-profile.js
@@ -18,49 +18,87 @@ function guardarInfoUsuario() {
     usuario_correo = document.getElementById("correo-usuario").value.trim();
     usuario_telefono = document.getElementById("telefono-usuario").value.trim();
 
-//Está bien que llame ahí a esa función?
-    crearJSON();
+
+    //Al tocar el botón del formulario, se muestra la tarjeta del perfil y se esconde el formulario
+    document.getElementById("tarjeta-perfil").style.display= "block";
+    document.getElementById("formulario-perfil").style.display= "none";
+
+    datos = {
+        nombre: usuario_nombre,
+        apellidos: usuario_apellidos,
+        edad: usuario_edad,
+        correo: usuario_correo,
+        telefono: usuario_telefono
+    }
+    
+    localStorage.setItem("miperfil", JSON.stringify(datos));
+    crearTarjPerfil();
+    // let datosTexto = localStorage.getItem("miperfil");
+    // datosRegistro = JSON.parse(datosTexto);
 }
 
-function crearJSON() {
-datos = {
-    nombre: usuario_nombre,
-    apellidos: usuario_apellidos,
-    edad: usuario_edad,
-    correo: usuario_correo,
-    telefono: usuario_telefono
-}
+// function crearJSON() {
+// datos = {
+//     nombre: usuario_nombre,
+//     apellidos: usuario_apellidos,
+//     edad: usuario_edad,
+//     correo: usuario_correo,
+//     telefono: usuario_telefono
+// }
 
-localStorage.setItem("miperfil", JSON.stringify(datos));
-let datosTexto = localStorage.getItem("miperfil");
-datosRegistro = JSON.parse(datosTexto);
+// localStorage.setItem("miperfil", JSON.stringify(datos));
+// let datosTexto = localStorage.getItem("miperfil");
+// datosRegistro = JSON.parse(datosTexto);
 
-crearTarjPerfil();
+// crearTarjPerfil();
 
-}
+// }
 
 function crearTarjPerfil() {
+    datosRegistro = JSON.parse(localStorage.getItem("miperfil"))
+    if (datosRegistro !== undefined) {
     let appendHTML = "";
-console.log(datosRegistro);
-console.log(datosRegistro.nombre)
 
-appendHTML = `<div class="card" style="width: 18rem;">
+appendHTML = `<div class="card" style="margin-left: auto; margin-right: auto; width: 25%;">
 <img class="card-img-top" src="..." alt="Card image cap">
 <div class="card-body">
     <h5 class="card-title">${datosRegistro.nombre} ${datosRegistro.apellidos}</h5>
 </div>
 <ul class="list-group list-group-flush">
-<li class="list-group-item" >Edad:${datosRegistro.edad}</li>
-<li class="list-group-item">Correo electrónico:${datosRegistro.correo}</li>
-<li class="list-group-item">Teléfono de contacto:${datosRegistro.telefono}</li>
+<li class="list-group-item">Edad: ${datosRegistro.edad}</li>
+<li class="list-group-item">Correo electrónico: ${datosRegistro.correo}</li>
+<li class="list-group-item">Teléfono de contacto: ${datosRegistro.telefono}</li>
 </ul>
+<button type="button" class="btn btn-primary" onclick="editarInfo()">Editar información</button>
 </div>
+
 `
-    document.getElementById("insertar-perfil").innerHTML = appendHTML;
+    document.getElementById("tarjeta-perfil").innerHTML = appendHTML;
+} }
+
+function editarInfo() {
+    document.getElementById("tarjeta-perfil").style.display= "none";
+    document.getElementById("formulario-perfil").style.display= "block";
+}
+
+function comprobarLocalStorage() {
+
+if (localStorage.getItem("miperfil") !== undefined && localStorage.getItem("miperfil"))
+{
+    document.getElementById("tarjeta-perfil").style.display= "block";
+    document.getElementById("formulario-perfil").style.display= "none";
+
+}
+    
+    else {
+        document.getElementById("tarjeta-perfil").style.display= "none";
+        document.getElementById("formulario-perfil").style.display= "block";
+    }
 }
 
 
-
 document.addEventListener("DOMContentLoaded", function (e) {
-
+    comprobarLocalStorage();
+    crearTarjPerfil();
+   
 })
