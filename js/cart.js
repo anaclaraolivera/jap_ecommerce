cb95aeefaeb574331f0caffbc1c06523ad2b12eb
anaclaraolivera <acolivera13@gmail.com>
2021-11-10 21:14:15 -0300

    muchos cambioos
    se cambia que el valor del input de cantidad tenga que ser si o si uno o mayor a uno
    se agrega a la funcion de selectcurrency que si no selecciono moneda les aparezca una alerta
    se crea la funcion actualizar costos que lo que hace es calcular el costo del envío en función del porcentaje del tipo de envío elegido
    function validar campos - esta va validando los campos y si no ponen algo salen alerts
    function guardarmetodopago - esta lo que hace es ver que metodo de pago selecciono la persona y lo muestra en pantalla, siempre y cuando haya llenado los campos requeridos de ese metodo
    function tarjetaclick- se fija si selecciona el metodo de tarjeta de credito, lo que aparece en la otra opcion no se puede modificar
    y function transferclick hace lo contrario

--- js/cart.js
+++ js/cart.js
@@ -1,11 +1,15 @@
 let allArticles = [];
 let subtotalPesos = 0;
 let subtotalDolares = 0;
+let subtotalFinal = 0;
+let porcentEnvio = 0.15;
+let cantidadArticulos = 0;
 
 const DOLARES = "USD";
 const PESOS = "UYU";
 
 
+//Función que crea el carrito a traves de los elementos del JSON
 function showCarrito() {
 
   let contenidoHTML = "";
@@ -38,10 +42,9 @@ function actualizarSubtotal() {
     cantidad = document.getElementById("cantidad-producto" + i).value;
     costo = allArticles[i].unitCost;
 
-    if (cantidad <= 0) {
+    if (cantidad <= 1) {
 
-      document.getElementById("cantidad-producto" + i).value = 0;
-      document.getElementById("subtotal-producto" + i).innerText = 0;
+      document.getElementById("cantidad-producto" + i).value = 1;
 
     }
     else {
@@ -50,11 +53,12 @@ function actualizarSubtotal() {
     }
   }
   calcularSubtotal();
+  // actualizarCostos()
 }
 
 function calcularSubtotal() {
-  subtotalPesos= 0;
-  subtotalDolares= 0;
+  subtotalPesos = 0;
+  subtotalDolares = 0;
 
   for (let i = 0; i < allArticles.length; i++) {
 
@@ -72,24 +76,137 @@ function calcularSubtotal() {
   }
   document.getElementById("total-pago-dolares").innerText = subtotalDolares;
   document.getElementById("total-pago-pesos").innerText = subtotalPesos;
+  // actualizarCostos()
 }
 
 function selectCurrency(option) {
   if (option === DOLARES) {
     document.getElementById("total-pago-dolares").style.display = "block";
     document.getElementById("total-pago-pesos").style.display = "none";
+    //sirve para borrar el texto que aparece si no apretó moneda
+    //se borra al elegir una moneda
+    document.getElementById("texto-no-moneda").style.display ="none";
   }
   else {
     document.getElementById("total-pago-pesos").style.display = "block";
     document.getElementById("total-pago-dolares").style.display = "none";
+    //sirve para borrar el texto que aparece si no apretó moneda
+    //se borra al elegir una moneda
+    document.getElementById("texto-no-moneda").style.display ="none";
+  }
+  // Acá lo que hace es ver con qué moneda se quedó, y a partir de eso se calcula el subtotal para el envío
+  if (option === DOLARES) {
+    subtotalFinal = document.getElementById("total-pago-dolares").innerText;
+  }
+  else {
+    subtotalFinal = document.getElementById("total-pago-pesos").innerText;
+  }
+  actualizarCostos();
+}
+//problema es q solo se actualiza cuando toco el botón de cambiar moneda
+function actualizarCostos() {
+  //Obtiene el valor del subtotal si es pesos o dólares
+  subtotal = parseInt(subtotalFinal);
+  envio = parseInt(subtotal * porcentEnvio);
+  total = parseInt(subtotal + envio);
+
+  document.getElementById("costo-subt").innerHTML = subtotal;
+  document.getElementById("costo-envio").innerHTML = envio;
+  document.getElementById("costo-total").innerHTML = total;
+
+}
+function validarCampos() {
+  let direccion = document.getElementById("direccion-dir").value;
+  let numero = document.getElementById("numero-dir").value;
+  let esquina = document.getElementById("esquina-dir").value;
+  let subPESOS = document.getElementById("total-pago-pesos").style.display ="none";
+  let subDOLARES = document.getElementById("total-pago-dolares").style.display ="none";
+  if (direccion === "" || numero === "" || esquina === "") {
+    alert ("Debes llenar los campos de dirección de envío")
+   if(direccion === "") {
+     document.getElementById("no-dir").style.visibility = "visible";
+   }
+   if(numero === "") {
+    document.getElementById("no-num").style.visibility = "visible";
+   }
+   if (esquina === "") {
+    document.getElementById("no-esq").style.visibility = "visible";
+   }
+  }
+  if ( subPESOS|| subDOLARES) {
+    document.getElementById("texto-no-moneda").style.display ="block";
+    alert("Debes elegir una moneda para continuar")
+  }
+} 
+
+// Función que se fija cuál es el método de pago seleccionado y muestra en pantalla 
+//Además se cerciora de que haya ingresado datos dentro del método de pago seleccionado antes de dar guardar
+function guardarMetodoPago() {
+  let tarjeta = document.getElementById("radio-tarj-credito").checked;
+  let transferencia = document.getElementById("radio-transf").checked;
+  if (tarjeta === true && transferencia === false) {
+document.getElementById("forma-pago-tarjeta").style.display = "block";
+if ( document.getElementById("numero-tarj").value === "" ||
+document.getElementById("codigo-tarj").value === "" ||
+document.getElementById("vencimiento-tarj").value === "") {
+  document.getElementById("forma-pago-tarjeta").style.display = "none";
+  alert ("Tienes que llenar todos los campos")
+}
+if (document.getElementById("numero-tarj").value === "") {
+  document.getElementById("no-num-tarj").style.visibility = "visible";
+}
+if (document.getElementById("codigo-tarj").value === "") {
+  document.getElementById("no-cod").style.visibility = "visible";
+}
+if (document.getElementById("vencimiento-tarj").value === "") {
+  document.getElementById("no-venc").style.visibility = "visible";
+}
+}
+  else if (tarjeta === false && transferencia === true){
+    document.getElementById("forma-pago-transferencia").style.display = "block";
+    if (document.getElementById("numero-transf").value === "") {
+      document.getElementById("forma-pago-transferencia").style.display = "none";
+      alert ("Tienes que llenar todos los campos")
+    }
+    if (document.getElementById("numero-transf").value ==="") {
+      document.getElementById("no-cuenta").style.visibility = "visible";
+    }
   }
 }
+ // document.getElementById("no-cod").style.visibility = "visible";
+  // document.getElementById("no-venc").style.visibility = "visible";
 
+
+//al clickear ese radio button se deshabilita el input de la transf y se habilitan los de la tarj
+function tarjetaClick() {
+  document.getElementById("numero-transf").readOnly = true;
+  document.getElementById("numero-tarj").readOnly = false;
+  document.getElementById("codigo-tarj").readOnly = false;
+  document.getElementById("vencimiento-tarj").readOnly = false;
+//acá se asegura de limpiar los campos del otro lugar
+  document.getElementById('numero-transf').value = '';
+  document.getElementById("no-cuenta").style.visibility = "hidden";
+  
+}
+//al clickear este se deshabilitan los input de la tarj y se habilitan los de la trasnf
+function transferClick() {
+  document.getElementById("numero-tarj").readOnly = true;
+  document.getElementById("codigo-tarj").readOnly = true;
+  document.getElementById("vencimiento-tarj").readOnly = true;
+  document.getElementById("numero-transf").readOnly = false;
+//acá se asegura de limpiar los campos del otro lugar
+  document.getElementById('numero-tarj').value = '';
+  document.getElementById('codigo-tarj').value = '';
+  document.getElementById('vencimiento-tarj').value = '';
+  document.getElementById("no-num-tarj").style.visibility = "hidden";
+  document.getElementById("no-cod").style.visibility = "hidden";
+  document.getElementById("no-venc").style.visibility = "hidden";
+}
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-//Desafiate 5, el json tiene dos artículos 
+  //Desafiate 5, el json tiene dos artículos 
   getJSONData(CART_2PROD_INFO).then(function (jsonCarrito) {
     if (jsonCarrito.status === "ok") {
       allArticles = jsonCarrito.data.articles;
@@ -104,8 +221,21 @@ document.addEventListener("DOMContentLoaded", function (e) {
   document.getElementById("dólares").addEventListener("click", function () {
     selectCurrency(DOLARES);
   });
+  //Botones radio para elegir el tipo de envio 
+  document.getElementById("envio-premium").addEventListener("change", function () {
+    porcentEnvio = 0.15;
+    actualizarCostos();
+  });
+
+  document.getElementById("envio-express").addEventListener("change", function () {
+    porcentEnvio = 0.07;
+    actualizarCostos();
+  });
+
+  document.getElementById("envio-standard").addEventListener("change", function () {
+    porcentEnvio = 0.05;
+    actualizarCostos();
+  });
+
 });
-
-
-
 
